98fc10d53ed6372183fb5e709c20457c7c069d0f
Iran Mas <imas@fvi.edu>
2017-08-09 19:35:08 -0400

    move winner; reset the game

--- app.js
+++ app.js
@@ -2,14 +2,59 @@ const state = require('./state.js')
 
 const dieContainer = document.querySelector('.die-container')
 
-function movePlayer(tileNumber){
-    state.player1.count += tileNumber
+function resetGame(){
+    player1.style.top = '160px'
+    player1.style.left ='10px'
+    player2.style.top = '250px'
+    player2.style.left = '10px'
 
-    const tilePosition = document.querySelector('#tile' + state.player1.count).getBoundingClientRect()
+    state.player1.count = 0
+    state.player2.count = 0
 
-    player1.style.top = tilePosition.top + 'px'
-    player1.style.left = tilePosition.left +'px'
+    player1.classList.remove('winner')
+    player2.classList.remove('winner')
+
+    state.currentPlayer = (Math.random() > .5) ? 'player1' : 'player2'
 }
+
+function moveWinner(){
+    const winnerCirclePosition = document.querySelector('#winner-circle').getBoundingClientRect()
+    state[state.currentPlayer].element.style.top = winnerCirclePosition.top + 'px'
+    state[state.currentPlayer].element.style.left = winnerCirclePosition.left +'px'
+
+    state[state.currentPlayer].element.classList.add('winner')
+
+    setTimeout(function(){
+        alert(`${state.currentPlayer} is the winner`)
+        if(confirm ('Reset the game?')){
+            resetGame()
+        }
+    }, 401)
+}
+
+
+
+
+function movePlayer(tileNumber){
+    state[state.currentPlayer].count += tileNumber
+
+    if(state[state.currentPlayer].count > 10){
+        moveWinner()
+        return
+    }
+
+    const tilePosition = document.querySelector('#tile' + state[state.currentPlayer].count).getBoundingClientRect()
+
+    state[state.currentPlayer].element.style.top = tilePosition.top + 'px'
+    state[state.currentPlayer].element.style.left = tilePosition.left +'px'
+
+    if(state.currentPlayer === 'player1'){
+        state.currentPlayer = 'player2'
+    } else {
+        state.currentPlayer = 'player1'
+    }
+}
+
 
 dieContainer.addEventListener('click', function(){
     die.setAttribute('src', 'img/Dodecahedron3.gif')
--- state.js
+++ state.js
@@ -1,11 +1,13 @@
 const state = {
     player1: {
-        count: 0
+        count: 0,
+        element: player1
     },
     player2: {
-        count: 0
+        count: 0,
+        element: player2
     },
-    currentPlayer: 'player1'
+    currentPlayer: (Math.random() > 5) ? 'player1' : 'player2'
 }
 
 module.exports = state
